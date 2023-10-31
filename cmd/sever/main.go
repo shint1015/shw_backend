9853c9617447bcb51eabd8ca9aecb1c6a27d6016
shint1015 <53039633+shint1015@users.noreply.github.com>
2023-10-31 23:17:18 +0900

    [update]

--- cmd/sever/main.go
+++ cmd/sever/main.go
@@ -1,55 +1,127 @@
 package main
 
 import (
+	"connectrpc.com/connect"
+	"connectrpc.com/grpcreflect"
 	"context"
 	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/rs/cors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"log"
-	"net"
-	"os"
-	"os/signal"
+	"net/http"
+	"shwgrpc/http/controller"
 	shwgrpc "shwgrpc/pkg/grpc"
+	"shwgrpc/pkg/grpc/grpcconnect"
 )
 
+var houseworkController = controller.NewHouseworkController()
+var familyController = controller.NewFamilyController()
+var userController = controller.NewUserController()
+
 type ShwServer struct {
-	shwgrpc.UnimplementedHelloServiceServer
-	shwgrpc.UnimplementedHouseworkServiceServer
-	shwgrpc.UnimplementedFamilyServiceServer
-	shwgrpc.UnimplementedUserServiceServer
+	//shwgrpc.UnimplementedHelloServiceServer
+	//shwgrpc.UnimplementedHouseworkServiceServer
+	//shwgrpc.UnimplementedFamilyServiceServer
+	//shwgrpc.UnimplementedUserServiceServer
+	grpcconnect.UnimplementedHelloServiceHandler
+	grpcconnect.UnimplementedHouseworkServiceHandler
+	grpcconnect.UnimplementedFamilyServiceHandler
+	grpcconnect.UnimplementedUserServiceHandler
 }
 
 func NewShwServer() *ShwServer {
 	return &ShwServer{}
 }
 
-func main() {
-	port := 8080
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-
-	if err != nil {
-		panic(err)
-	}
-
-	s := grpc.NewServer()
-
-	shwgrpc.RegisterHelloServiceServer(s, NewShwServer())
-
-	reflection.Register(s)
-
-	go func() {
-		log.Printf("gRPC server is running on port %d", port)
-		s.Serve(listener)
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-
-	log.Println("Shutting down gRPC server...")
-	s.GracefulStop()
+func newServeMuxWithReflection() *http.ServeMux {
+	mux := http.NewServeMux()
+	reflector := grpcreflect.NewStaticReflector(
+		"shw.HelloService",
+		"shw.HouseworkService",
+		"shw.FamilyService",
+		"shw.UserService",
+	)
+	mux.Handle(grpcreflect.NewHandlerV1(reflector))
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(reflector))
+	return mux
 }
 
-func (s *ShwServer) Hello(ctx context.Context, req *shwgrpc.HelloRequest) (*shwgrpc.HelloResponse, error) {
-	return &shwgrpc.HelloResponse{Message: "Hello " + req.Name}, nil
+func newInterceptors() connect.Option {
+	interceptors := func(next connect.UnaryFunc) connect.UnaryFunc {
+		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+			req.Header().Set("hoge", "fuga")
+			return next(ctx, req)
+		})
+	}
+	return connect.WithInterceptors(connect.UnaryInterceptorFunc(interceptors))
+}
+
+func main() {
+	//fmt.Println("Hello World")
+	//domain := "localhost"
+	port := 8080
+	shw := NewShwServer()
+	//mux := newServeMuxWithReflection()
+	interceptor := newInterceptors()
+	mux := newServeMuxWithReflection()
+	mux.Handle(grpcconnect.NewFamilyServiceHandler(shw, interceptor))
+	mux.Handle(grpcconnect.NewHouseworkServiceHandler(shw, interceptor))
+	mux.Handle(grpcconnect.NewUserServiceHandler(shw, interceptor))
+	mux.Handle(grpcconnect.NewHelloServiceHandler(shw, interceptor))
+
+	//mux.Handle(NewHouse)
+	log.Printf("gRPC server is running on port %d", port)
+	corsHandler := makeCorsHandler()
+	err := http.ListenAndServe(
+		fmt.Sprintf(":%d", port),
+		corsHandler.Handler(h2c.NewHandler(mux, &http2.Server{})),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	log.Println("Shutting down gRPC server...")
+
+	//listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//s := grpc.NewServer()
+	//
+	//shwgrpc.RegisterHelloServiceServer(s, NewShwServer())
+	//shwgrpc.RegisterFamilyServiceServer(s, NewShwServer())
+	//shwgrpc.RegisterHouseworkServiceServer(s, NewShwServer())
+	//shwgrpc.RegisterUserServiceServer(s, NewShwServer())
+	//
+	//reflection.Register(s)
+	//
+	//go func() {
+	//	log.Printf("gRPC server is running on port %d", port)
+	//	s.Serve(listener)
+	//}()
+	//
+	//quit := make(chan os.Signal, 1)
+	//signal.Notify(quit, os.Interrupt)
+	//<-quit
+	//
+	//log.Println("Shutting down gRPC server...")
+	//s.GracefulStop()
+}
+
+func makeCorsHandler() *cors.Cors {
+	// TODO: Originを環境変数から取得する
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://172.19.0.2:3000", "http://localhost", "http://localhost:3000"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"*"},
+	})
+	return corsHandler
+}
+
+func (s *ShwServer) Hello(ctx context.Context, req *connect.Request[shwgrpc.HelloRequest]) (*connect.Response[shwgrpc.HelloResponse], error) {
+	res := connect.NewResponse(&shwgrpc.HelloResponse{Message: "Hello " + req.Msg.Name})
+	return res, nil
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,39 @@ module shwgrpc
 go 1.21.1
 
 require (
+	connectrpc.com/connect v1.11.1 // indirect
+	connectrpc.com/grpcreflect v1.2.0 // indirect
+	github.com/bufbuild/connect-go v1.7.0 // indirect
+	github.com/bufbuild/connect-grpcreflect-go v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/labstack/echo-jwt/v4 v4.2.0 // indirect
+	github.com/labstack/echo/v4 v4.11.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
 	google.golang.org/grpc v1.58.2 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
