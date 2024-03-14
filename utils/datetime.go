79a2df0ed7ff481168d5c2c1c04d25333cf89edc
shint1015 <53039633+shint1015@users.noreply.github.com>
2024-03-14 23:21:57 +0900

    [update] fix bug

--- utils/datetime.go
+++ utils/datetime.go
@@ -33,7 +33,7 @@ func GetExpireTime() time.Time {
 	return time.Now().Add(time.Hour * 3)
 }
 
-func Str2Time(str string) Time.time {
+func Str2Time(str string) time.Time {
 	layout := "2006-01-02 15:04:05 MST"
 	parsedTime, _ := time.Parse(layout, str)
 	return parsedTime
